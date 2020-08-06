1acdcec0988bfd29f2919925873157f371b0769a
a-powers <awpowers@gmail.com>
2020-08-05 21:43:06 -0400

    update

--- iteration-with-the-for-loop.py
+++ iteration-with-the-for-loop.py
@@ -1,48 +1,64 @@
-dinner = "Steak and Potatoes"
+# dinner = "Steak and Potatoes"
 
-for character in dinner:
-    print(character)
-
-
-numbers = [2, 3, 5, 7, 10]
-
-for number in numbers:
-    print(number * number)
+# for character in dinner:
+#     print(character)
 
 
+# numbers = [2, 3, 5, 7, 10]
 
-
-novelists = ["Fitzgerald", "Hemingway", "Steinbeck"]
-
-for novelist in novelists:
-    print(len(novelist))
+# for number in numbers:
+#     print(number * number)
 
 
 
 
-total = 0
+# novelists = ["Fitzgerald", "Hemingway", "Steinbeck"]
 
-for number in numbers:
-    total = total + number
-print(total)
-
-
-
-# # Coding Exercise 18
-
-def sum_of_lengths(chars):
-    counting = 0
-    for char in chars:
-        counting = counting + len(char)
-    return(counting)
-print(sum_of_lengths(["Frogs", "Toads"]))
+# for novelist in novelists:
+#     print(len(novelist))
 
 
 
-def product(numbas):
-    counts = 1
-    for num in numbas:
-        counts = counts * num
-    return(counts)
-print(product([1, 2, 3, 4, 5]))
-    
+
+# total = 0
+
+# for number in numbers:
+#     total = total + number
+# print(total)
+
+
+
+# # # Coding Exercise 18
+
+# def sum_of_lengths(chars):
+#     counting = 0
+#     for char in chars:
+#         counting = counting + len(char)
+#     return(counting)
+# print(sum_of_lengths(["Frogs", "Toads"]))
+
+
+
+# def product(numbas):
+#     counts = 1
+#     for num in numbas:
+#         counts = counts * num
+#     return(counts)
+# print(product([1, 2, 3, 4, 5]))
+
+
+
+def total_lenght(my_list):
+    start_the_list = 0
+    for word in my_list:
+        start_the_list = start_the_list + len(word)
+    return(start_the_list)
+print(total_lenght(["Boo", "Too"]))
+
+
+def product(the_numbers):
+    the_list = 1
+    for value in the_numbers:
+        the_list = the_list * value
+    return(the_list)
+print(product([1, 2, 3]))
--- the-range-function.py
+++ the-range-function.py
@@ -2,14 +2,22 @@
 #     print(number)
 
 
-for number in range(0, 11):
-    print(number)
-
-
-# for number in range(10, 101, 10):
+# for number in range(0, 11):
 #     print(number)
 
 
-for number in range(99, -109, -11):
-    print(number)
-    
+# # for number in range(10, 101, 10):
+# #     print(number)
+
+
+# for number in range(99, -109, -11):
+#     print(number)
+
+
+
+
+
+
+
+
+
