344920a83d74b523a142c10ae28d4174bf08cb2c
a-powers <awpowers@gmail.com>
2020-08-20 20:36:04 -0400

    update

--- iteration-with-the-for-loop.py
+++ iteration-with-the-for-loop.py
@@ -26,39 +26,3 @@
 #     total = total + number
 # print(total)
 
-
-
-# # # Coding Exercise 18
-
-# def sum_of_lengths(chars):
-#     counting = 0
-#     for char in chars:
-#         counting = counting + len(char)
-#     return(counting)
-# print(sum_of_lengths(["Frogs", "Toads"]))
-
-
-
-# def product(numbas):
-#     counts = 1
-#     for num in numbas:
-#         counts = counts * num
-#     return(counts)
-# print(product([1, 2, 3, 4, 5]))
-
-
-
-def total_lenght(my_list):
-    start_the_list = 0
-    for word in my_list:
-        start_the_list = start_the_list + len(word)
-    return(start_the_list)
-print(total_lenght(["Boo", "Too"]))
-
-
-def product(the_numbers):
-    the_list = 1
-    for value in the_numbers:
-        the_list = the_list * value
-    return(the_list)
-print(product([1, 2, 3]))
